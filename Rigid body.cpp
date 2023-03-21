9cd7c2ac7c3aee72ae321d614b5f9ed22696f2f6
snabrew <125982822+snabrew@users.noreply.github.com>
2023-03-21 13:20:37 +0000

    how friction works

--- Rigid body.cpp
+++ Rigid body.cpp
@@ -13,7 +13,7 @@ Vector3 normal (Vector3 force, Vector3 norm){
     float m = uF.dot(un);
     //normalised dot products are basically cos theta.
     
-    return force * m * -1;
+    return force * m;
 }
 
 Vector3 weight ( point_mass* p){
@@ -22,11 +22,12 @@ Vector3 weight ( point_mass* p){
     return g * (p->getMass()) ;
 }
 
-//Vector3 friction ( float coef,Vector3 norm){}
-
-float torque ( Vector3 force, Vector3 r){
-    // r is the vector from the center of geometry to the contact point.
-    return (force.cross(r)).magnitude();
+Vector3 friction ( float coef,Vector3 norm,Vector3 vel){
+    Vector3 dir = -1 * vel.normalise();
+    Vector3 ff = coef * norm;
+    ff = ff.magnitude() * dir;
     
+    return ff;
 }
+
 
--- Rigid body.hpp
+++ Rigid body.hpp
@@ -2,3 +2,6 @@
 #pragma once
 #include "vector3.h"
 
+Vector3 normal (Vector3 force, Vector3 norm);
+Vector3 weight ( point_mass* p);
+Vector3 friction ( float coef,Vector3 norm);
