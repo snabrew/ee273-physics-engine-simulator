72a8407fdb5150ac459973c2137bc9f242a71ecf
uwaisdin <125983379+uwaisdin@users.noreply.github.com>
2023-04-03 14:32:54 +0100

    Add files via upload

---
+++ work/updated-work/glutSims.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Vector3.h"
+#include "point_mass.h"
+#include "sphere.h"
+
+void drawPlane();
+void display();
+void init();
+void reshape(int w, int h);
+void idle();
+void bouncing_ball();
+void collision_balls();
